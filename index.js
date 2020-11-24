c91bf44c748a2bdecbee1e95b9772e39def8c4c3
Nikita Bukin <locus2@mail.ru>
2020-11-24 15:53:16 +0300

    async issue

--- index.js
+++ index.js
@@ -7,43 +7,69 @@
 // });
 const fs = require('fs');
 const request = require('request');
-const {JSDOM} = require('jsdom');
+const {
+    JSDOM
+} = require('jsdom');
 
-let URL = 'https://ranobes.com/chapters/against-the-gods/102105-glava-1227-gorenie-krovi-feniksa.html';
+let url = 'https://ranobes.com/chapters/against-the-gods/102105-glava-1227-gorenie-krovi-feniksa.html';
 
-request(URL, function (err, res, body) {
-    if (err) throw err;
-    const dom = new JSDOM(body);
-    let pageText = dom.window.document.querySelector('#arrticle').innerHTML;
-    let pageTitle = dom.window.document.querySelector('.h4').innerHTML;
-
-    fs.writeFileSync("dom.html", pageText);
-    
-    console.log(res.statusCode);
-    rwTitle(pageTitle);
-    rwText();
-});
-
-function rwTitle(pageTitle) {
-    pageTitle = pageTitle.replace(/\<span(.*)/gims, '');
-    fs.appendFileSync('dataReplaced.html', `\n${pageTitle}\n`, 'utf-8');
+for (let i = 0; i < 10; i++) {
+    console.log('url1: ', url);
+    getText();
+    console.log('url2: ', url);
+    // setTimeout(nextPush(), 3000);
+    console.log('url3: ', url);
 }
 
-// Регулярки можно записывать подряд к одному файлу
-function rwText () {
-    let data = fs.readFileSync('dom.html', 'utf-8');
-    // Убираем неразрывные пробелы
-    data = data.replace(/\&nbsp\;/gi, ' '); 
-    // Двойной перенос строки
-    data = data.replace(/<br><br>/gi, '\n');
-    // Непонятный див со скриптом
-    data = data.replace(/<div(.*)\/div>/gims, ' ');
-    fs.appendFileSync('dataReplaced.html', data, 'utf-8');
-    clearSrc();
-    console.log('complete');
-}
+function getText() {
 
-// Удаляем излишки
-function clearSrc() {
-    fs.unlinkSync("dom.html");
+    request(url, function (err, res, body) {
+        if (err) {
+            throw err;
+        }
+        const dom = new JSDOM(body);
+        let pageText = dom.window.document.querySelector('#arrticle').innerHTML;
+        let pageTitle = dom.window.document.querySelector('.h4').innerHTML;
+        
+        let nextPage = dom.window.document.querySelector('#next');
+        // console.log('nextPage: ', nextPage);
+        // console.log('nextPage: ', nextPage.href);
+        url = nextPage.href;
+
+        fs.writeFileSync("dom.html", pageText);
+
+        console.log(res.statusCode);
+        rwTitle(pageTitle);
+        rwText();
+         return url;
+    });
+
+    function rwTitle(pageTitle) {
+        pageTitle = pageTitle.replace(/(<span)(.*)/gims, '');
+        fs.appendFileSync('dataReplaced.html', `\n${pageTitle}\n`, 'utf-8');
+    }
+
+    // Регулярки можно записывать подряд к одному файлу
+    function rwText() {
+        let data = fs.readFileSync('dom.html', 'utf-8');
+        // Убираем неразрывные пробелы
+        data = data.replace(/\&nbsp\;/gi, ' ');
+        // Двойной перенос строки
+        data = data.replace(/<br><br>/gi, '\n');
+        // Непонятный див со скриптом
+        data = data.replace(/<div(.*)\/div>/gims, ' ');
+        fs.appendFileSync('dataReplaced.html', data, 'utf-8');
+        clearSrc();
+        console.log('complete');
+    }
+
+    // Удаляем излишки
+    function clearSrc() {
+        fs.unlinkSync("dom.html");
+    }
+}
+// getText();
+
+function nextPush(nextPage) {
+    nextPage.click();
 }
